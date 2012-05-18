c9cffe4396ad655ed8a1266339445b3ce05ee321
Freewind <nowind_lee@qq.com>
2012-05-18 22:13:55 +0800

    demo for sortBy

---
+++ sortBy.js
@@ -0,0 +1,34 @@
+var async = require('async');
+
+var t = require('./t');
+var log = t.log;
+
+/**
+ * 对集合内的元素进行排序，依据每个元素进行某异步操作后产生的值，从小到大排序。
+ */
+// sortBy(array, iterator(item,callback(err,result)), callback(err,results))
+
+var arr = [3,6,1];
+
+async.sortBy(arr, function(item, callback) {
+    setTimeout(function() {
+        callback(null,item);
+    }, 200);
+}, function(err,results) {
+    log('1.1 err: ', err);
+    log('1.1 results: ', results);
+});
+// 26.562> 1.1 err: null
+// 26.562> 1.1 results: [ 1, 3, 6 ]
+
+async.sortBy(arr, function(item, callback) {
+    setTimeout(function() {
+        if(item===6) callback('myerr');
+        else callback(null,item);
+    }, 200);
+}, function(err,results) {
+    log('1.2 err: ', err);
+    log('1.2 results: ', results);
+});
+// 26.572> 1.2 err: myerr
+// 26.572> 1.2 results:
