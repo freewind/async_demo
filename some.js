b1fbd8d0d5caec173d4d90ad66d69b5b461b0c55
Freewind <nowind_lee@qq.com>
2012-05-18 22:14:17 +0800

    demo for some

---
+++ some.js
@@ -0,0 +1,58 @@
+var async = require('async');
+
+var t = require('./t');
+var log = t.log;
+
+/**
+ * 当集合中是否有至少一个元素满足条件时，最终callback得到的值为true，否则为false.
+ */
+// some(arr, iterator(item,callback(test)), callback(result))
+//alias: any
+
+var arr = [1,2,3,6];
+
+/**
+ * 集合中至少有一个元素<=3，所以结果为true
+ */
+// 1.1
+async.some(arr, function(item,callback){
+    log('1.1 enter: ',item);
+    setTimeout(function(){
+        log('1.1 handle: ',item);
+        callback(item<=3);
+    },100);    
+}, function(result) {
+    log('1.1 result: ', result);
+});
+// 36.165> 1.1 enter: 1
+// 36.165> 1.1 enter: 2
+// 36.165> 1.1 enter: 3
+// 36.165> 1.1 enter: 6
+// 36.275> 1.1 handle: 1
+// 36.275> 1.1 result: true
+// 36.275> 1.1 handle: 2
+// 36.275> 1.1 handle: 3
+// 36.275> 1.1 handle: 6
+
+
+/**
+ * 集合中没有一个元素>10，所以结果为false
+ */
+async.some(arr, function(item,callback){
+    log('1.2 enter: ',item);
+    setTimeout(function(){
+        log('1.2 handle: ',item);
+        callback(item>10);
+    },100);    
+}, function(result) {
+    log('1.2 result: ', result);
+});
+// 36.165> 1.2 enter: 1
+// 36.165> 1.2 enter: 2
+// 36.165> 1.2 enter: 3
+// 36.165> 1.2 enter: 6
+// 36.275> 1.2 handle: 1
+// 36.275> 1.2 handle: 2
+// 36.275> 1.2 handle: 3
+// 36.275> 1.2 handle: 6
+// 36.275> 1.2 result: false
