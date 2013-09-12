292c3a1d033254c6ff94bad3e80bafa2b363c59d
bsspirit <bsspirit@163.com>
2013-09-12 10:57:39 +0800

    reject

--- filter_reject.js
+++ filter_reject.js
@@ -28,23 +28,25 @@ async.filter(arr, function(item, callback) {
 }, function(results) {
     log('1.1 results: ', results);
 });
-// 16.739> 1.1 enter: 1
-// 16.749> 1.1 enter: 2
-// 16.749> 1.1 enter: 3
-// 16.749> 1.1 enter: 4
-// 16.749> 1.1 enter: 5
-// 16.749> 1.3 enter: 1
-// 16.949> 1.1 test: 1
-// 16.949> 1.1 test: 2
-// 16.949> 1.1 test: 3
-// 16.949> 1.1 test: 4
-// 16.949> 1.1 test: 5
-// 16.949> 1.1 results: [ 3, 4, 5 ]
+//16.739> 1.1 enter: 1
+//16.749> 1.1 enter: 2
+//16.749> 1.1 enter: 3
+//16.749> 1.1 enter: 4
+//16.749> 1.1 enter: 5
+//16.749> 1.3 enter: 1
+//16.949> 1.1 test: 1
+//16.949> 1.1 test: 2
+//16.949> 1.1 test: 3
+//16.949> 1.1 test: 4
+//16.949> 1.1 test: 5
+//16.949> 1.1 results: [ 3, 4, 5 ]
 
 
 /**
- * 如果出错，将会由nodejs抛出，导致出错。为保证其它代码正常运行，注释掉该测试。
- */
+* 如果出错，将会由nodejs抛出，导致出错。为保证其它代码正常运行，注释掉该测试。
+*
+* try..catch：抓不到这个错误
+*/
 /*
 async.filter(arr, function(item, callback) {
     log('1.2 enter: ' + item);
@@ -61,8 +63,8 @@ async.filter(arr, function(item, callback) {
 */
 
 /**
- * 并行执行，对arr进行筛选。
- */
+* 串行执行，对arr进行筛选。
+*/
 // 1.3
 async.filterSeries(arr, function(item, callback) {
     log('1.3 enter: ' + item);
@@ -87,8 +89,8 @@ async.filterSeries(arr, function(item, callback) {
 
 
 /*
- * reject跟filter正好相反，当测试为true时，抛弃之
- */
+* reject跟filter正好相反，当测试为true时，抛弃之
+*/
 // reject(arr, iterator(item, callback(test)), callback(results)
 async.reject(arr, function(item, callback) {
     log('1.4 enter: ' + item);
@@ -110,3 +112,29 @@ async.reject(arr, function(item, callback) {
 // 31.559> 1.4 test: 4
 // 31.559> 1.4 test: 5
 // 31.569> 1.4 results: [ 1, 2 ]
+
+
+/**
+ * 串行执行，对arr进行筛选。
+ */
+// 1.3
+async.rejectSeries(arr, function(item, callback) {
+    log('1.5 enter: ' + item);
+    setTimeout(function() {
+        log('1.5 handle: ' + item);
+        callback(item>=3);
+    }, 200);
+}, function(results) {
+    log('1.5 results: ', results);
+});
+//43.592> 1.5 enter: 1
+//43.799> 1.5 handle: 1
+//43.800> 1.5 enter: 2
+//44.004> 1.5 handle: 2
+//44.007> 1.5 enter: 3
+//44.210> 1.5 handle: 3
+//44.211> 1.5 enter: 4
+//44.412> 1.5 handle: 4
+//44.413> 1.5 enter: 5
+//44.614> 1.5 handle: 5
+//44.616> 1.5 results: [ 1, 2 ]
